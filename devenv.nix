002330c575cd389d55ff21200d4d290051f2dc4e
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-06-02 17:25:20 -0300

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 {
+  languages.python = {
+    enable = true;
+    venv = {
+      enable = true;
+      requirements = ''
+        letterboxdpy
+      '';
+    };
+  };
+
   languages.typescript.enable = true;
 
   languages.javascript = {
